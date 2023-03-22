5bc90f07f69ede248caf0a2d25a841f7591a4771
muskan-bajaj <muskanvijaybajaj@gmail.com>
2023-03-22 20:06:58 +0530

    hashing

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -1,16 +1,29 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
   },
 });
+
+userSchema.statics.signUpUser = async function (email, password) {
+  //generating salt
+  const salt = await bcrypt.genSalt(10);
+
+  //hashing password
+  const hash = await bcrypt.hash(password, salt);
+
+  const newUser = await this.create({ email, password: hash });
+  return newUser;
+};
 
 userSchema.statics.loginUser = async function (email, password) {
   if (!email || !password) {
